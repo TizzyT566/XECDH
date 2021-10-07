de67caee7804fc2bd207a4bc245927332074ca00
Thien Huynh <tizzyt566@gmail.com>
2021-10-07 16:45:43 -0400

    initial commit

---
+++ XECDH/XECDiffieHellmanCng.cs
@@ -0,0 +1,94 @@
+﻿using System.IO;
+
+namespace System.Security.Cryptography
+{
+    public static class XECDiffieHellmanCng
+    {
+        public static CngKey GenerateECDiffieHellmanP521Key()
+        {
+            CngKeyCreationParameters cngKeyCreationParameters = new CngKeyCreationParameters { ExportPolicy = CngExportPolicies.AllowPlaintextArchiving };
+            return CngKey.Create(CngAlgorithm.ECDiffieHellmanP521, null, cngKeyCreationParameters);
+        }
+
+        public static void SaveEncryptedEccFullPrivateBlob(this CngKey @this, string path, string passphrase)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.BlockSize = 128;
+                aes.Padding = PaddingMode.PKCS7;
+                using (SHA384 passPhraseToKeyIV = new SHA384Managed())
+                {
+                    byte[] keyiv = passPhraseToKeyIV.ComputeHash(Text.Encoding.UTF8.GetBytes(passphrase));
+                    byte[] key = new byte[32];
+                    Array.Copy(keyiv, 0, key, 0, 32);
+                    byte[] iv = new byte[16];
+                    Array.Copy(keyiv, 0, iv, 32, 16);
+                    aes.Key = key;
+                    aes.IV = iv;
+                }
+                using var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                File.WriteAllBytes(path, PerformCryptography(@this.Export(CngKeyBlobFormat.EccFullPrivateBlob), encryptor));
+            };
+        }
+
+        public static CngKey LoadEncryptedEccFullPrivateBlob(string path, string passphrase)
+        {
+            using (var aes = Aes.Create())
+            using (SHA384 passPhraseToKeyIV = new SHA384Managed())
+            {
+                aes.KeySize = 256;
+                aes.BlockSize = 128;
+                aes.Padding = PaddingMode.PKCS7;
+
+                byte[] keyiv = passPhraseToKeyIV.ComputeHash(Text.Encoding.UTF8.GetBytes(passphrase));
+                byte[] key = new byte[32];
+                Array.Copy(keyiv, 0, key, 0, 32);
+                byte[] iv = new byte[16];
+                Array.Copy(keyiv, 0, iv, 32, 16);
+                aes.Key = key;
+                aes.IV = iv;
+
+                using var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                return CngKey.Import(PerformCryptography(File.ReadAllBytes(path), decryptor), CngKeyBlobFormat.EccFullPrivateBlob);
+            };
+        }
+
+        private static byte[] PerformCryptography(byte[] data, ICryptoTransform cryptoTransform)
+        {
+            using var ms = new MemoryStream();
+            using var cryptoStream = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write);
+            cryptoStream.Write(data, 0, data.Length);
+            cryptoStream.FlushFinalBlock();
+            return ms.ToArray();
+        }
+
+        public static byte[] DeriveKey(ECDiffieHellmanCng personalECDH, ECDiffieHellmanCng ephemeralECDH, ECDiffieHellmanPublicKey otherPartyPublicKey, ECDiffieHellmanPublicKey otherPartyEphemeralPublicKey, CngAlgorithm innerHashAlgorithm)
+        {
+            byte[] keyA, keyB;
+            ephemeralECDH.HashAlgorithm = personalECDH.HashAlgorithm = innerHashAlgorithm;
+            ephemeralECDH.KeyDerivationFunction = personalECDH.KeyDerivationFunction;
+            keyA = personalECDH.DeriveKeyMaterial(otherPartyEphemeralPublicKey);
+            keyB = ephemeralECDH.DeriveKeyMaterial(otherPartyPublicKey);
+            static bool KeySort(byte[] keyA, byte[] keyB)
+            {
+                for (int i = 0; i < keyA.Length; i++)
+                    if (keyA[i] < keyB[i]) return true;
+                    else if (keyA[i] > keyB[i]) return false;
+                return true;
+            }
+            byte[] derivedKey = new byte[keyA.Length + keyB.Length];
+            if (KeySort(keyA, keyB))
+            {
+                Array.Copy(keyA, 0, derivedKey, 0, keyA.Length);
+                Array.Copy(keyB, 0, derivedKey, keyA.Length, keyB.Length);
+            }
+            else
+            {
+                Array.Copy(keyB, 0, derivedKey, 0, keyB.Length);
+                Array.Copy(keyA, 0, derivedKey, keyB.Length, keyA.Length);
+            }
+            return derivedKey;
+        }
+    }
+}
---
+++ XECDiffieHellmanCngTest/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+using CngKey tempKey = XECDiffieHellmanCng.GenerateECDiffieHellmanP521Key();
+
+tempKey.SaveEncryptedEccFullPrivateBlob(@"C:\Users\tizzy\Desktop\cngkey.enc", "pokemon");
+CngKey keyA = XECDiffieHellmanCng.LoadEncryptedEccFullPrivateBlob(@"C:\Users\tizzy\Desktop\cngkey.enc", "pokemon");
+
+
+
+ECDiffieHellmanCng personal = new(keyA);
+
+
+
+using CngKey keyB = XECDiffieHellmanCng.GenerateECDiffieHellmanP521Key();
+ECDiffieHellmanCng ephemeral = new(keyB);
+
+
+
+using CngKey theirKeyA = CngKey.Create(CngAlgorithm.ECDiffieHellmanP521);
+ECDiffieHellmanCng theirPersonal = new(theirKeyA);
+using CngKey theirKeyB = CngKey.Create(CngAlgorithm.ECDiffieHellmanP521);
+ECDiffieHellmanCng theirEphemeral = new(theirKeyB);
+
+
+
+byte[] derivedKeyA = XECDiffieHellmanCng.DeriveKey(personal, ephemeral, theirPersonal.PublicKey, theirEphemeral.PublicKey, CngAlgorithm.Sha512);
+byte[] derivedKeyB = XECDiffieHellmanCng.DeriveKey(theirPersonal, theirEphemeral, personal.PublicKey, ephemeral.PublicKey, CngAlgorithm.Sha512);
+
+Console.WriteLine(derivedKeyA.SequenceEqual(derivedKeyB));
+Console.WriteLine(derivedKeyA.Length * 8);
+
+foreach (byte b in derivedKeyA) Console.Write($"{b:000}, ");
+Console.WriteLine();
+Console.WriteLine();
+foreach (byte b in derivedKeyB) Console.Write($"{b:000}, ");
+
+Console.WriteLine();
+Console.WriteLine();
+
+derivedKeyA = XECDiffieHellmanCng.DeriveKey(personal, ephemeral, theirPersonal.PublicKey, theirEphemeral.PublicKey, CngAlgorithm.Sha512);
+derivedKeyB = XECDiffieHellmanCng.DeriveKey(theirPersonal, theirEphemeral, personal.PublicKey, ephemeral.PublicKey, CngAlgorithm.Sha512);
+
+Console.WriteLine(derivedKeyA.SequenceEqual(derivedKeyB));
+Console.WriteLine(derivedKeyA.Length * 8);
+
+foreach (byte b in derivedKeyA) Console.Write($"{b:000}, ");
+Console.WriteLine();
+Console.WriteLine();
+foreach (byte b in derivedKeyB) Console.Write($"{b:000}, ");
+
+Console.ReadLine();
